ee5ec709196b1516ee8eaee5c0abd0b34e0f5596
BemakorSimon <43002710+BemakorSimon@users.noreply.github.com>
2018-10-01 05:32:31 -0700

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,23 @@
+
+public class OperationsPractice 
+{
+
+	public static void main(String[] args) 
+	{
+		int newNumber;
+		newNumber = 12;
+		System.out.println("Number =: " + newNumber);
+		newNumber += 5;
+		System.out.println("Add 5: " + newNumber);
+		newNumber /= 2;
+		System.out.println("Divide by 2: " + newNumber);
+		newNumber *= 4;
+		System.out.println("Multiply by 4: " + newNumber);
+		newNumber -= 6;
+		System.out.println("Subtract 6: " + newNumber);
+		newNumber %= 2;
+		System.out.print("Modualo divide by 2: " + newNumber);
+		
+	}
+
+}
